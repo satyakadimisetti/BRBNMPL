5da3bbb853b109e7445ece23efc3fc85a3247c5b
satyanarayana <kadimisettisatya1@gmail.com>
2025-03-31 11:12:35 +0530

    Initial commit

---
+++ src/main/java/com/Bank/BRBNMPL/BrbnmplApplication.java
@@ -0,0 +1,13 @@
+package com.Bank.BRBNMPL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrbnmplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrbnmplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bank/BRBNMPL/BrbnmplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bank.BRBNMPL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrbnmplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
